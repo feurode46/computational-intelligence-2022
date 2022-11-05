25ab057d4c0e8931efda7c1073054a6cef21aa07
Federico Boscolo <s294908@studenti.polito.it>
2022-11-05 18:11:23 +0100

    works pretty well with 100, a solution for 500

--- lab2/EA.py
+++ lab2/EA.py
@@ -2,8 +2,9 @@ import random
 
 class EvolutionaryModel:
 
-    def __init__(self, problem, pop_size=100, offspring_size=50, epochs=10000, fitness_evaluations_threshold=10000):
+    def __init__(self, problem, N=5, pop_size=100, offspring_size=2, epochs=10000, fitness_evaluations_threshold=10000):
         self.problem = problem
+        self.N = N
         self.problem_size = len(self.problem)
         self.population_size = pop_size
         self.offspring_size = offspring_size
@@ -13,6 +14,7 @@ class EvolutionaryModel:
         self.fitness_evaluations = 0
         self.fitness_evaluations_threshold = fitness_evaluations_threshold
         self.mutation_chance = 0.2
+
         # generate initial individuals
         for i in range(self.population_size):
             self.population.append(self.generate_individual())
@@ -20,11 +22,22 @@ class EvolutionaryModel:
 
 
     def generate_individual(self):
-        #todo : select initital popolation with higher fitness
         genome = tuple([random.choice([0, 1]) for _ in range(self.problem_size)])
         fitness = self.fitness_eval(genome)
         individual = tuple([genome, fitness])
         return individual
+    
+    # def generate_individual_smart(self):
+    #     self.problem = sorted(self.problem)
+    #     # since lists with fewer values are at the start, try to include them more in starting population
+    #     genome = list()
+    #     genome.extend([random.choice([0, 1, 1, 1]) for _ in range(int(self.problem_size/4))])
+    #     genome.extend([random.choice([0, 1]) for _ in range(int(self.problem_size/4), int(self.problem_size/2))])
+    #     genome.extend([random.choice([0, 0, 0, 1]) for _ in range(int(self.problem_size/2), int(self.problem_size))])
+    #     genome = tuple(genome)
+    #     fitness = self.fitness_eval(genome)
+    #     individual = tuple([genome, fitness])
+    #     return individual
 
 
     def fitness_eval(self, genome):
@@ -35,7 +48,7 @@ class EvolutionaryModel:
         n_el = 0
         for i in range(len(genome)):
             if genome[i] == 1:
-                n_el += len(self.problem[i])
+                n_el += len(self.problem[i]*genome[i])
                 # count number of digits in that list
                 for j in self.problem[i]:
                     if j not in digits:
@@ -53,8 +66,7 @@ class EvolutionaryModel:
         # cut = random.randint(0, self.problem_size)
         # return g1[:cut] + g2[cut:]
         g3 = list()
-        for i in range(self.problem_size):
-            g3.append(random.choice([g1[i], g2[i]]))
+        g3.extend([random.choice([g1[i], g2[i]]) for i in range(self.problem_size)])
         return tuple(g3)
 
     def mutate(self, genome):
@@ -68,7 +80,7 @@ class EvolutionaryModel:
                 new_genome[idx] = 0
         return tuple(new_genome)
 
-    def evolute(self):
+    def evolve(self):
         for i in range(self.offspring_size):
             if random.randint(1, 10) < 10*self.mutation_chance:
                 offspring_genome = self.mutate(random.choice(self.population)[0])
@@ -83,7 +95,7 @@ class EvolutionaryModel:
     
     def simulate(self):
         for i in range(self.epochs):
-            self.evolute()
+            self.evolve()
             if i % (self.epochs/20) == 0:
                 print(f"{int(i/self.epochs*100)}%")
             if (self.fitness_evaluations > self.fitness_evaluations_threshold):
@@ -93,31 +105,14 @@ class EvolutionaryModel:
     
     def sort_population(self):
         self.population = sorted(self.population, key=lambda i: i[1], reverse=True)
-    
-    def best_solution(self):
-        return self.population[0]
-    
+
     def converted_solution(self):
+        if (self.population[0][1][0] < self.N):
+            print("No solution has been found!")
+            return list()
         solution = list()
-        genome = self.best_solution()[0]
+        genome = self.population[0][0]
         for i in range(len(genome)):
             if genome[i] == 1:
                 solution.append(self.problem[i])
         return solution
-
-
-    # def fitness_eval_old(self, genome):
-    #     # tuple with: 1) number of digits included, 2) total n. of elements, 3) number of lists selected
-    #     # 1 must be as high as possible, 2 and 3 as low as possible
-    #     # 2: -> return total length - n. selected
-    #     # 3: -> return number of lists NOT selected -> must be as high as possible
-    #     digits = list()
-    #     n_el = 0
-    #     for i in range(self.problem_size):
-    #         if genome[i] == 1:
-    #             n_el += len(self.problem[i])
-    #             # count number of digits in that list
-    #             for j in self.problem[i]:
-    #                 if j not in digits:
-    #                     digits.append(j)
-    #     return tuple([len(digits), self.total_length - n_el, self.problem_size - sum(genome)])
--- lab2/main.py
+++ lab2/main.py
@@ -16,16 +16,16 @@ if __name__ == "__main__":
     Ns = [5, 10, 20, 100, 500, 1000]
     for N in Ns:
         ls = problem(N, 42)
-        random.shuffle(ls)
         # print(ls)
         print(f"working on N={N}...")
-        ea = EvolutionaryModel(ls, pop_size=100, offspring_size=2, epochs=10000)
+        ea = EvolutionaryModel(ls, N=N, pop_size=150, offspring_size=100, epochs=1000, fitness_evaluations_threshold=10000)
         # print(ea.population, sep="\n")
         ea.simulate()
         solution = ea.converted_solution()
         print(
             f"Solution for N={N}: w={sum(len(_) for _ in solution)} (bloat={(sum(len(_) for _ in solution) - N) / N * 100:.0f}%)"
         )
-        print(f"{solution}")
+        if N <= 20:
+            print(f"{solution}")
         print("---------")
 
