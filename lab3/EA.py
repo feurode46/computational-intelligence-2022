efe34297ff1ef8b153f80e31239ab0ef7410492d
Federico Boscolo <federicoboscolo@icloud.com>
2022-12-04 22:52:05 +0100

    evolving

---
+++ lab3/EA.py
@@ -0,0 +1,153 @@
+import random
+from nim import Nim, Nimply
+
+
+class EvolutionaryModel:
+
+    def __init__(self, nim_size=5, pop_size=150, offspring_size=100, epochs=1000, fitness_evaluations_threshold=10000, mutation_chance = 0.2, num_matches=100):
+        self.N = nim_size
+        self.num_matches = num_matches
+        self.population_size = pop_size
+        self.offspring_size = offspring_size
+        self.epochs = epochs
+        self.population = list()
+        self.fitness_evaluations = 0
+        self.fitness_evaluations_threshold = fitness_evaluations_threshold
+        self.mutation_chance = mutation_chance
+
+        # generate initial population
+        for i in range(self.population_size):
+            self.population.append(self.generate_individual())
+            self.sort_population()
+
+    def generate_individual(self):
+        # for Nim: an individual is a (genome, fitness) tuple
+        # genome: probability threshold of choosing all / all but one matches
+        # from arbitrary row, in case of even or odd number of rows
+
+        genome = tuple([random.random(), random.random()])
+        fitness = self.fitness_eval(genome)
+        individual = tuple([genome, fitness])
+        # print(individual)
+        return individual
+
+    def cook_status(self, state: Nim) -> dict:
+        cooked = dict()
+        cooked["active_rows"] = list([o for o in enumerate(state.rows) if o[1] > 0])
+        cooked["n_active_rows"] = len(cooked["active_rows"])
+        cooked["rows_2_or_more"] = list([o for o in cooked["active_rows"] if o[1] > 1])
+
+        return cooked
+
+    def evolved_strategy(self, genome, state: Nim) -> Nimply:
+        data = self.cook_status(state)
+        probs = [random.random() for i in range(2)]
+        if data["n_active_rows"] % 2 == 0:
+            if len(data["rows_2_or_more"]) > 0:
+                row = data["rows_2_or_more"][0][0]
+                if probs[0] > genome[0]:
+                    n_objects = data["rows_2_or_more"][0][1]  # take all
+                else:
+                    n_objects = data["rows_2_or_more"][0][1] - 1  # leave one
+            else:
+                # forced move: take one from arbitrary row
+                row = data["active_rows"][0][0]
+                n_objects = 1
+        else:
+            if len(data["rows_2_or_more"]) > 0:
+                row = data["rows_2_or_more"][0][0]
+                if probs[1] > genome[1]:
+                    n_objects = data["rows_2_or_more"][0][1]  # take all
+                else:
+                    n_objects = data["rows_2_or_more"][0][1] - 1  # leave one
+            else:
+                # forced move: take one
+                row = data["active_rows"][0][0]
+                n_objects = 1
+
+        return Nimply(row, n_objects)
+
+    def random_strategy(self, state: Nim) -> Nimply:
+        row = random.choice([r for r, c in enumerate(state.rows) if c > 0])
+        num_objects = random.randint(1, state.rows[row])
+        return Nimply(row, num_objects)
+
+    def fitness_eval(self, genome):
+        # win rate over 100 matches
+        self.fitness_evaluations += 1
+        won = 0
+        for m in range(self.num_matches):
+            nim = Nim(self.N)
+            player = 0
+            while nim:
+                if player == 0:
+                    ply = self.evolved_strategy(genome, nim)
+                else:
+                    ply = self.random_strategy(nim)
+                nim.nimming(ply)
+                player = 1 - player
+            if player == 1:
+                won += 1
+        return won / self.num_matches
+
+    def select_parent(self, tournament_size=2):
+        return max(random.choices(self.population, k=tournament_size), key=lambda i: i[1])
+
+    def cross_over(self, g1, g2):
+        return tuple([g1[0], g2[1]])
+
+    def mutate(self, genome):
+        # increment or decrement a random probability by 0.05
+        r1 = random.random()
+        r2 = random.random()
+        p1 = genome[0]
+        p2 = genome[1]
+        if r1 > 0.5:
+            if r2 > 0.5:
+                p1 -= 0.05
+                if p1 < 0:
+                    p1 = 0
+            else:
+                p1 += 0.05
+                if p1 > 1:
+                    p1 = 1
+        else:
+            if r2 > 0.5:
+                p2 -= 0.05
+                if p2 < 0:
+                    p2 = 0
+            else:
+                p2 += 0.05
+                if p2 > 1:
+                    p2 = 1
+        new_genome = tuple([p1, p2])
+        return new_genome
+
+    def evolve(self):
+        for i in range(self.offspring_size):
+            if random.random() < self.mutation_chance:
+                offspring_genome = self.mutate(random.choice(self.population)[0])
+            else:
+                p1 = self.select_parent()
+                p2 = self.select_parent()
+                offspring_genome = self.cross_over(p1[0], p2[0])
+            self.population.append(tuple([offspring_genome, self.fitness_eval(offspring_genome)]))
+        # then sort and trim
+        self.sort_population()
+        self.population = self.population[:self.population_size]
+
+    def simulate(self):
+        for i in range(self.epochs):
+            self.evolve()
+            if self.fitness_evaluations % (self.fitness_evaluations_threshold/10) == 0:
+                print(f"{int(self.fitness_evaluations/self.fitness_evaluations_threshold*100)}%")
+            if self.fitness_evaluations > self.fitness_evaluations_threshold:
+                print(f"Maximum number of evaluations ({self.fitness_evaluations}) reached. Trained for {i} generations.")
+                break
+        print("100% Done!")
+
+    def sort_population(self):
+        self.population = sorted(self.population, key=lambda i: i[1], reverse=True)
+
+    def best_genome(self):
+        return self.population[0][0]
--- lab3/main.py
+++ lab3/main.py
@@ -5,14 +5,13 @@ import random
 from copy import deepcopy
 from itertools import accumulate
 from operator import xor
-
+from EA import EvolutionaryModel
 
 NUM_MATCHES = 100
-NIM_SIZE = 3
+NIM_SIZE = 5
 
 
 def cook_status(state: Nim) -> dict:
-    # change this!
     cooked = dict()
     cooked["possible_moves"] = [
         (r, o) for r, c in enumerate(state.rows) for o in range(1, c + 1) if state.k is None or o <= state.k
@@ -53,27 +52,24 @@ def random_strategy(state: Nim) -> Nimply:
 def my_strategy(state: Nim) -> Nimply:
     # odd number of active rows: take whole row (more than 1 element if possible)
     cooked_state = cook_status(state)
-    # print(cooked_state)
-    row = 0
-    n_objects = 1
-    if cooked_state["n_active_rows"] % 2 == 1:
-        if len(cooked_state["rows_2_or_more"]) > 0:
-            row = cooked_state["rows_2_or_more"][0][0]  # index of row
+
+    # default: take whole 1st row
+    row = cooked_state["active_rows"][0][0]        # row index
+    n_objects = cooked_state["active_rows"][0][1]  # row value
+
+    if len(cooked_state["rows_2_or_more"]) > 0:
+        if cooked_state["n_active_rows"] % 2 == 1:
+            row = cooked_state["rows_2_or_more"][0][0]
             n_objects = cooked_state["rows_2_or_more"][0][1]
         else:
-            row = cooked_state["active_rows"][0][0]
-            n_objects = 1
-    else:
-        # take all but one (temp. strategy)
-        if len(cooked_state["rows_2_or_more"]) > 0:
             row = cooked_state["rows_2_or_more"][0][0]
             n_objects = cooked_state["rows_2_or_more"][0][1] - 1
 
     return Nimply(row, n_objects)
 
 
-def evaluate(strategy: Callable) -> float:
-    opponent = (strategy, random_strategy)
+def evaluate(strategy1: Callable, strategy2: Callable) -> float:
+    opponent = (strategy1, strategy2)
     won = 0
 
     for m in range(NUM_MATCHES):
@@ -81,6 +77,31 @@ def evaluate(strategy: Callable) -> float:
         player = 0
         while nim:
             ply = opponent[player](nim)
+            nim.nimming(ply)
+            player = 1 - player
+        if player == 1:
+            won += 1
+    return won / NUM_MATCHES
+
+
+def evaluate_EA(ea, strategy, reverse=False):
+
+    # win rate over 100 matches
+    won = 0
+    for m in range(NUM_MATCHES):
+        nim = Nim(NIM_SIZE)
+        player = 0
+        while nim:
+            if reverse:
+                if player == 1:
+                    ply = ea.evolved_strategy(ea.best_genome(), nim)
+                else:
+                    ply = strategy(nim)
+            else:
+                if player == 0:
+                    ply = ea.evolved_strategy(ea.best_genome(), nim)
+                else:
+                    ply = strategy(nim)
             nim.nimming(ply)
             player = 1 - player
         if player == 1:
@@ -103,8 +124,49 @@ def make_strategy(genome: dict) -> Callable:
     return evolvable
 
 
+def sample_game(n, strategy1: Callable, strategy2: Callable) -> None:
+    strategy = (strategy1, strategy2)
+    nim = Nim(n)
+    print(f"status: Initial board  -> {nim}")
+    print(f"Player 1: {strategy[0].__name__}")
+    print(f"Player 2: {strategy[1].__name__}")
+    player = 0
+    while nim:
+        ply = strategy[player](nim)
+        nim.nimming(ply)
+        print(f"status: After player {player + 1} -> {nim}")
+        player = 1 - player
+    winner = 1 - player
+    print(f"status: Player {strategy[winner].__name__} won!")
+
+
 if __name__ == "__main__":
-    mynim = Nim(3)
-    # print(mynim.rows)
-    print(f"Performance against random strategy, {NUM_MATCHES} games: {evaluate(my_strategy)*100}%")
+    print("Training evolutionary algorithm...")
+    ea = EvolutionaryModel(NIM_SIZE, 150, 100, 100, 10000, 0.2, NUM_MATCHES)
+    ea.simulate()
+    best = ea.best_genome()
+    print(best)
+    ea_strategies = [
+        my_strategy,
+        random_strategy,
+        optimal_strategy
+    ]
+
+    for s in ea_strategies:
+        print(f"{ea.evolved_strategy.__name__} vs. {s.__name__} {evaluate_EA(ea, s) * 100}%")
+        print(f"{s.__name__} vs. {ea.evolved_strategy.__name__} {evaluate_EA(ea, s, reverse=True) * 100}%")
+
+    strategy_pairs = [
+        (my_strategy, random_strategy),
+        (my_strategy, optimal_strategy)
+    ]
+    # sample_game(NIM_SIZE, my_strategy, random_strategy)
+
+    print(f"--- Size={NIM_SIZE}, games={NUM_MATCHES} ---")
+    for p in strategy_pairs:
+        print(f"{p[0].__name__} vs. {p[1].__name__} {evaluate(p[0], p[1]) * 100}%")
+        print(f"{p[1].__name__} vs. {p[0].__name__} {evaluate(p[1], p[0]) * 100}%")
+
+
+
 
